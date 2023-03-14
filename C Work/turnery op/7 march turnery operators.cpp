fdfa2afb72bf8f144429e21a53053996d89ec3bd
komalvadariya <komalvadariya0802@gmail.com>
2023-03-14 16:47:06 +0530

    work

---
+++ C Work/turnery op/7 march turnery operators.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+int a,b;
+printf("1 number: ");
+scanf("%d",&a);
+printf("2 number: ");
+scanf("%d",&b);	
+
+a>b?(printf("a is bigger then b")):(printf("b is bigger then a"));
+}
