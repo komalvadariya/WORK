656275ea74af355cfd0df3db8de33d7565b4872b
komalvadariya <komalvadariya0802@gmail.com>
2023-03-14 16:32:46 +0530

    work

---
+++ C Work/assignmnetop/4 march assignmnet op.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+{
+	//we can use all the arithmatic like +,-,*,/ 
+	int a=5;
+	a-=2;
+	printf("%d\n",a);
+	a*=2;
+	printf("%d\n",a);
+	a+=5;
+	printf("%d\n",a);
+	a/=2;
+	printf("%d\n",a);
+}
