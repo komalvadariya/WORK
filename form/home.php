5c02a1142385c99f15f98f66579ef4ed43e6c895
komalvadariya <komalvadariya0802@gmail.com>
2023-07-08 09:44:44 +0530

    PHP

---
+++ form/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <section class="home" id="home">
+        <div class="container">
+            <div class="row">
+                <h1>komal</h1>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
