b5decaffc6345d3b33f189f53a14f7392f6d104a
komalvadariya <komalvadariya0802@gmail.com>
2023-09-11 12:35:42 +0530

    TEST

---
+++ Test_SE/Q-11.cpp
@@ -0,0 +1,43 @@
+//11. Write a program to sort the array of 5 elements. 
+#include <stdio.h>
+
+int main()
+{
+    int arr1[100];
+    int n, i, j, tmp;
+	
+	
+       printf("sort elements of array in ascending order :\n ");
+       	
+
+    printf("Input the size of array : ");
+    scanf("%d", &n);
+
+       printf("Input %d elements in the array :\n",n);
+       for(i=0;i<n;i++)
+            {
+	      printf("element - %d : ",i);
+	      scanf("%d",&arr1[i]);
+	    }
+
+    for(i=0; i<n; i++)
+    {
+        for(j=i+1; j<n; j++)
+        {
+            if(arr1[j] <arr1[i])
+            {
+                tmp = arr1[i];
+                arr1[i] = arr1[j];
+                arr1[j] = tmp;
+            }
+        }
+    }
+    printf("\nElements of array in sorted ascending order:\n");
+    for(i=0; i<n; i++)
+    {
+        printf("%d  ", arr1[i]);
+    }
+	        printf("\n\n");
+}
+
+
